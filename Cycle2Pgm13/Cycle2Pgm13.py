528268365142882802ff7ea42c52d7a90f371150
NehmaM <93966525+NehmaM@users.noreply.github.com>
2021-12-16 13:52:57 +0530

    Add files via upload

---
+++ Cycle2Pgm13/Cycle2Pgm13.py
@@ -0,0 +1,13 @@
+filename = raw_input("Input the filename")
+extn = filename.split(".")
+print("The extension of file is : " + extn[-1])
+
+
+
+
+
+'''
+f = open(filename, mode)
+mode=r,w,a,r+,w+,a+
+
+'''
