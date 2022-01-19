37fc311995e7e02615607499a975458af4d036b0
NehmaM <93966525+NehmaM@users.noreply.github.com>
2022-01-19 07:59:49 +0530

    Add files via upload

---
+++ Cycle3Pgm21/Cycle3Pgm21.py
@@ -0,0 +1,10 @@
+#python program to find the factorial of a number using functions.
+
+def factorial(num):
+    fact=1
+    for i in range(1,num+1):
+        fact=fact*i
+    return fact
+number = int(input("Enter number to find factorial"))
+result = factorial(number)
+print("The factorial of %d=%d"%(number,result))
