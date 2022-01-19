dd225a8e91feee3de311a0c7c5a7d3c6be610de9
NehmaM <93966525+NehmaM@users.noreply.github.com>
2022-01-19 17:44:34 +0530

    Add files via upload

---
+++ Cycle2Pgm25/Cycle2Pgm25.py
@@ -0,0 +1,11 @@
+#python program to generate all factors of a number.
+
+def factor(x):
+    print("The factors of",x,"are:")
+    for i in range(1,x+1):
+        if x % i == 0:
+            print(i)
+
+
+num = int(input("Enter a number to generate all factors of the number"))
+factor(num)
