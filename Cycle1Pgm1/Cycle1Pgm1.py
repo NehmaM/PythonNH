75a16cfc137fdcdcadd156174b6f81e862b515c6
NehmaM <93966525+NehmaM@users.noreply.github.com>
2022-03-06 13:22:43 +0530

    Add files via upload

---
+++ Cycle1Pgm1/Cycle1Pgm1.py
@@ -0,0 +1,20 @@
+#Write a python program to find the area of a circle
+#area of circle=3.14*r*r
+
+
+def circle(r):
+    print("Area of circle=",3.14*r*r)
+
+r=float(input("Enter radius of the circle"))
+circle(r)
+
+
+'''
+import math
+def circle(r):
+    area = math.pi**r*r
+    print("Area of circle=",area)
+
+r=float(input("Enter radius of the circle"))
+circle(r)
+'''
